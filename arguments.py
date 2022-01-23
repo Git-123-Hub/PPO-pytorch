84774d2cf2a6c88ccbb536cdf37ccd10dec136ef
Git-123-Hub <2063487599@qq.com>
2022-01-23 21:01:15 +0800

    add arguments; move hyper-parameters to args; add running reward

---
+++ arguments.py
@@ -0,0 +1,23 @@
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='PPO')
+
+    parser.add_argument('--seed', type=int, default=None, help='random seed (default: None)')
+    parser.add_argument('--env-name', type=str, default='Pendulum-v0', help='name of the environment')
+    parser.add_argument('--total-steps', type=int, default=300000,
+                        help='total steps that interact with the environment during training')
+    parser.add_argument('--num-process', type=int, default=4, help='num of envs that run in parallel')
+    parser.add_argument('--learn-interval', type=int, default=2400,
+                        help='step interval between each learning')
+    parser.add_argument('--batch-size', type=int, default=600,
+                        help='number of experiences sampled for buffer each time learning')
+    parser.add_argument('--learn-epoch', type=int, default=30, help='epoch of learning when ppo update')
+    parser.add_argument('--clip-ratio', type=float, default=0.5, help='ppo clip parameter')
+    parser.add_argument('--learning-rate', type=float, default=3e-4, help='learning rate for actor-critic')
+    parser.add_argument('--clip-grad', type=float, default=0.5, help='clip gradient norm')
+    # todo: linear decay: lr and std for ac; hidden-layer;
+
+    args = parser.parse_args()
+    return args
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import math
+from collections import deque
 
 import gym
 import numpy as np
 import torch
+import torch.nn.functional as F
 from matplotlib import pyplot as plt
 from stable_baselines3.common.utils import set_random_seed
 from stable_baselines3.common.vec_env import SubprocVecEnv
-import torch.nn.functional as F
 from torch import optim, nn
 
 from ActorCritic import ActorCritic
 from Buffer import Buffer
+from arguments import get_args
 
 
 def make_env(env_id, rank, seed=0):
@@ -22,22 +24,18 @@ def make_env(env_id, rank, seed=0):
     return _init
 
 
-total_steps = 3e5
-num_process = 4  # num of env running in parallel
-learn_interval = 2400  # agent learn per 1e3 steps
-# Note that the buffer has to collect at least `learn_interval` experiences
-# consider `num_process`, envs should be interacted for `learn_interval/num_process` steps
-# which is also the capacity of buffer
-batch_size = 600
-learn_epoch = 30  # ppo learns 10 times each time learn
-clip_ratio = 0.5
-learning_rate = 3e-4  # for actor critic
-# set_random_seed(124132)
 rewards_to_plot = []
+running_rewards = []
+running_window = deque(maxlen=100)
 if __name__ == '__main__':
-    # set_random_seed(0)  # make the program reproducible
+    args = get_args()
+
+    if args.seed is not None:
+        set_random_seed(args.seed)  # make the program reproducible
+        print(f'seed has been set to {args.seed}')
+
     # set up envs
-    envs = SubprocVecEnv([make_env("Pendulum-v0", i) for i in range(num_process)])
+    envs = SubprocVecEnv([make_env(args.env_name, i) for i in range(args.num_process)])
 
     # set up actor critic
     state_size = envs.observation_space.shape[0]
@@ -52,34 +50,40 @@ if __name__ == '__main__':
         print(f'unknown action space')
     ac = ActorCritic(state_size, action_size)
     print(ac)
-    optimizer = optim.Adam(ac.parameters(), lr=learning_rate)
+    optimizer = optim.Adam(ac.parameters(), lr=args.learning_rate)
 
     # set up buffer
-    buffer = Buffer(int(learn_interval // num_process), int(batch_size))
+    # Note that the buffer has to collect at least `learn_interval` experiences
+    # consider `num_process`, envs should be interacted for `learn_interval/num_process` steps
+    # which is also the capacity of buffer
+    buffer = Buffer(int(args.learn_interval // args.num_process), int(args.batch_size))
 
-    learn_times = math.ceil(total_steps / learn_interval)
+    learn_times = math.ceil(args.total_steps / args.learn_interval)
     for n in range(learn_times):
         # initial buffer and envs
         buffer.reset()
         state = envs.reset()  # todo: how to handle next state
         buffer.states[0] = state  # initial state
-        episode_reward = [0] * num_process
+        current_reward = [0] * args.num_process
         episode_rewards = []
 
         # interact with envs
-        envs_steps = math.ceil(learn_interval / num_process)
+        envs_steps = math.ceil(args.learn_interval / args.num_process)
         # each time learn, envs should run for these steps to fill the buffer, i.e. prepare the data
         for _ in range(envs_steps):
             with torch.no_grad():
                 action, action_log_prob = ac.act(state)
                 state_value = ac.evaluate(state)
             state, rewards, dones, infos = envs.step(action)
-            episode_reward = [a + b for a, b in zip(episode_reward, rewards)]
+            # NOTE that `state` has been updated as the next state
+            current_reward = [a + b for a, b in zip(current_reward, rewards)]
             for i, done in enumerate(dones):
                 if done:
-                    rewards_to_plot.append(episode_reward[i])
-                    episode_rewards.append(episode_reward[i])
-                    episode_reward[i] = 0
+                    running_window.append(current_reward[i])
+                    running_rewards.append(np.mean(running_window))
+                    rewards_to_plot.append(current_reward[i])
+                    episode_rewards.append(current_reward[i])
+                    current_reward[i] = 0
 
             buffer.add(state, state_value.numpy(), action, action_log_prob.numpy(), rewards, dones)
             # NOTE that we are storing `next_state` and `current_state_value`
@@ -95,42 +99,42 @@ if __name__ == '__main__':
         # start to learn
         actor_loss_list = []
         critic_loss_list = []
-        for i in range(learn_epoch):
+        for i in range(args.learn_epoch):
             for states, state_values, actions, old_action_log_probs, advantages, discount_rewards, returns in buffer.data_generator():
                 # actor loss
                 action_log_probs = ac.get_action_log_prob(states, actions)
                 ratio = torch.exp(action_log_probs - old_action_log_probs)
-                ratio_clipped = torch.clip(ratio, 1 - clip_ratio, 1 + clip_ratio)
+                ratio_clipped = torch.clip(ratio, 1 - args.clip_ratio, 1 + args.clip_ratio)
                 actor_loss = -torch.min(ratio * advantages, ratio_clipped * advantages).mean()
                 assert actor_loss.requires_grad
                 actor_loss_list.append(float(actor_loss.data))
 
                 # critic loss
                 new_state_values = ac.evaluate(states)  # todo: seems the state values from batch is not used
-                # critic_loss = F.mse_loss(new_state_values, returns, reduction='mean')
+                critic_loss = F.mse_loss(new_state_values, returns, reduction='mean')
                 # critic_loss = F.mse_loss(new_state_values, advantages + state_values, reduction='mean')
-                critic_loss = F.mse_loss(new_state_values, discount_rewards, reduction='mean')
+                # critic_loss = F.mse_loss(new_state_values, discount_rewards, reduction='mean')
                 assert critic_loss.requires_grad
                 critic_loss_list.append(float(critic_loss.data))
 
                 # update actor critic
-                loss = actor_loss + critic_loss * 0.5
+                loss = actor_loss + critic_loss
                 optimizer.zero_grad()
                 loss.backward()
-                nn.utils.clip_grad_norm_(ac.parameters(), 0.5)  # todo: clip grad
+                nn.utils.clip_grad_norm_(ac.parameters(), args.clip_grad)
                 optimizer.step()
 
         buffer.reset()
-        print(f'{"-" * 10} update {n + 1}, experience collected: {buffer.size * num_process} {"-" * 10}\n'
-              f'{len(episode_rewards)} episodes finished, '
-              f'mean: {np.mean(episode_rewards): .4f}, std: {np.std(episode_rewards): .4f},'
-              f'actor loss: {np.mean(actor_loss_list): .4f}, critic loss: {np.mean(critic_loss_list): .4f}'
-              )
+        print(
+            f'{"-" * 10} update {n + 1}, experience collected: {buffer.size * args.num_process} {"-" * 10}\n'
+            f'{len(episode_rewards)} episodes finished, '
+            f'mean: {np.mean(episode_rewards): .4f}, std: {np.std(episode_rewards): .4f},'
+            f'actor loss: {np.mean(actor_loss_list): .4f}, critic loss: {np.mean(critic_loss_list): .4f}'
+        )
 
-    # plot episode reward
+    # training finishes, plot episode reward
     fig, ax = plt.subplots()
     ax.plot(range(1, len(rewards_to_plot) + 1), rewards_to_plot)
+    ax.plot(range(1, len(running_rewards) + 1), running_rewards)
     ax.hlines(-165, 1, len(rewards_to_plot) + 1, colors='red')
-    plt.savefig(f'Pendulum-v0 result.png')
-
-
+    plt.savefig(f'{args.env_name} result.png')
